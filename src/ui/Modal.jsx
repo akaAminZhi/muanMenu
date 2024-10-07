88405a1ab3c6768aa2fd3f0d9388996c04ab70d0
zhimin <min3832170@163.com>
2024-10-07 14:59:10 -0400

    modal can scroll

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -53,8 +53,8 @@ function Window({ children, name }) {
   if (openName !== name) return null;
   return createPortal(
     <div className="fixed top-0 left-0 w-full h-screen  backdrop-blur-sm transition duration-300 z-[1000]">
-      <div className="fixed min-w-96 border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-yellow-100  rounded-lg shadow-xl transition duration-500 px-4 py-2">
-        <button onClick={close}>
+      <div className="fixed min-w-96 border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-yellow-100  rounded-lg shadow-xl transition duration-500 px-4 py-2 max-h-[80vh] overflow-y-auto">
+        <button className="" onClick={close}>
           <HiXMark></HiXMark>
         </button>
         <div>{children}</div>
